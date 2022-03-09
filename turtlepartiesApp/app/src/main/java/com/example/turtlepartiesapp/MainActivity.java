07145efcb00160c0f307e58507db3caf74bbef3b
Dayyan Muhammad <dayyan@ualberta.ca>
2022-03-08 20:39:27 -0700

    completed map activity

--- turtlepartiesApp/app/src/main/java/com/example/turtlepartiesapp/MainActivity.java
+++ turtlepartiesApp/app/src/main/java/com/example/turtlepartiesapp/MainActivity.java
@@ -1,24 +1,33 @@
 package com.example.turtlepartiesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     final String TAG = "Sample";
     FirebaseFirestore db;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
     public void map_activity(View view) {
@@ -26,8 +35,9 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void profile_QR_activity() {
+    public void profile_QR_activity(View view) {
         Intent intent = new Intent(this, ProfileQRActivity.class);
         startActivity(intent);
     }
+
 }
--- turtlepartiesApp/app/src/main/java/com/example/turtlepartiesapp/MapsActivity.java
+++ turtlepartiesApp/app/src/main/java/com/example/turtlepartiesapp/MapsActivity.java
@@ -1,27 +1,84 @@
 package com.example.turtlepartiesapp;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback{
+
+    private String username = "test1";
     private GoogleMap mMap;
+    FirebaseFirestore db;
+    CollectionReference collectionReference;
+    private static final String TAG = MapsActivity.class.getSimpleName();
+    private FusedLocationProviderClient fusedLocationClient;
+    protected Double latitude,longitude;
 
+    @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
+        db = FirebaseFirestore.getInstance();
+
+        getPermission();
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
+                            Log.d(TAG, latitude + "  " +longitude);
+                        }
+                    }
+                });
+        Log.d(TAG, latitude + "  " +longitude);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -31,11 +88,81 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
 
-        LatLng ualberta = new LatLng(53.521331248, -113.521331248);
+        boolean success = googleMap.setMapStyle(new MapStyleOptions(getResources()
+                .getString(R.string.style_json)));
+
+        if (!success) {
+            Log.e(TAG, "Style parsing failed.");
+        }
+
+        Double latval = 53.521331248;
+        Double longval = -113.521331248;
+
+        if (longitude != null) {
+            setMapCenter(latitude, longitude, mMap);
+        }else{
+            setMapCenter(latval, longval, mMap);
+        }
+        addQRMarkers(mMap);
+    }
+
+    public void setMapCenter(Double latval, Double longval, @NonNull GoogleMap mMap){
+        LatLng center = new LatLng(latval, longval);
         mMap.addMarker(new MarkerOptions()
-                .position(ualberta)
-                .title("Ualberta"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(ualberta));
+                .position(center)
+                .title("You are here"))
+                .setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(center));
+    }
+
+    public void addQRMarkers(@NonNull GoogleMap googleMap){
+        mMap = googleMap;
+
+        collectionReference = db.collection("Users").document(username).collection("qrcodes");
+        collectionReference.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException error) {
+                for(QueryDocumentSnapshot doc: queryDocumentSnapshots)
+                {
+                    Double latval = ((Number) doc.getData().get("latval")).doubleValue();
+                    Double longval = ((Number) doc.getData().get("longval")).doubleValue();
+                    Integer score = ((Number) doc.getData().get("score")).intValue();
+
+                    Log.d(TAG, latval + "  " + longval + "  " + score);
+                    LatLng thisMarker = new LatLng(latval, longval);
+                    mMap.addMarker(new MarkerOptions()
+                            .position(thisMarker)
+                            .title(String.valueOf(score)));
+                }
+            }
+        });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void getPermission(){
+        ActivityResultLauncher<String[]> locationPermissionRequest =
+                registerForActivityResult(new ActivityResultContracts
+                                .RequestMultiplePermissions(), result -> {
+                            Boolean fineLocationGranted = result.getOrDefault(
+                                    Manifest.permission.ACCESS_FINE_LOCATION, false);
+                            Boolean coarseLocationGranted = result.getOrDefault(
+                                    Manifest.permission.ACCESS_COARSE_LOCATION,false);
+                            if (fineLocationGranted != null && fineLocationGranted) {
+                                Log.d(TAG, "FINE LOCATION");
+                            } else if (coarseLocationGranted != null && coarseLocationGranted) {
+                                Log.d(TAG, "COARSE LOCATION");
+                            } else {
+                                Log.d(TAG, "NO LOCATION");
+                            }
+                        }
+                );
+
+        locationPermissionRequest.launch(new String[] {
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        });
     }
 }
 
