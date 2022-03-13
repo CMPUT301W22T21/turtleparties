a719ab4a095d9bf24f29d3f973eb6b3b2b080bb2
Seojin3 <seojin3@ualberta.ca>
2022-03-13 14:10:12 -0600

    added fragements for login QR and friend QR

---
+++ turtlepartiesApp/app/src/main/java/com/example/turtlepartiesapp/LoginQRFragment.java
@@ -0,0 +1,24 @@
+package com.example.turtlepartiesapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class LoginQRFragment extends Fragment {
+
+    View view;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_login_qr, container, false);
+
+
+        return view;
+
+    }
+}
