888db58923a07bb6c7c006c1784b51ba6bca7ce6
Seojin3 <96707594+Seojin3@users.noreply.github.com>
2022-03-12 23:51:34 -0700

    Revert "Seojin"

--- turtlepartiesApp/app/src/main/java/com/example/turtlepartiesapp/MainActivity.java
+++ turtlepartiesApp/app/src/main/java/com/example/turtlepartiesapp/MainActivity.java
@@ -18,7 +18,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -63,15 +62,6 @@ public class MainActivity extends AppCompatActivity implements QRDeleteFragment.
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // added profile button
-        Button profileButton = (Button) findViewById(R.id.test_btn);
-        profileButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, ProfileActivity.class));
-            }
-        });
 
         context = this;
         checkAndRequestPermissions();
@@ -268,5 +258,4 @@ public class MainActivity extends AppCompatActivity implements QRDeleteFragment.
         Intent profileIntent = new Intent(this, ProfileQRActivity.class);
         startActivity(profileIntent);
     }
-
 }
