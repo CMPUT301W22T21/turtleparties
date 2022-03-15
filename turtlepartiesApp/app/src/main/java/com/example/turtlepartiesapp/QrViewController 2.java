82f07eeeb1e6eb049dd683aee9757f6295651b92
Shane Smith <shanesmith@Shanes-MacBook-Air.local>
2022-03-14 19:20:18 -0600

    commit

---
+++ turtlepartiesApp/app/src/main/java/com/example/turtlepartiesapp/QrViewController 2.java
@@ -0,0 +1,6 @@
+package com.example.turtlepartiesapp;
+
+public class QrViewController {
+    //implementation for this controller to be added
+    //this will be used to send data to the profileQRactivity
+}
