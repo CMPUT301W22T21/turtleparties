4160f42aa19b699dc96db6bf92bf5f14d9ef26e4
has <hassaan3@ualberta.ca>
2022-03-31 17:46:52 -0600

    adding leaderbord to depend on player controller

--- turtlepartiesApp/app/src/main/java/com/example/turtlepartiesapp/PlayerController.java
+++ turtlepartiesApp/app/src/main/java/com/example/turtlepartiesapp/PlayerController.java
@@ -1,16 +1,26 @@
 package com.example.turtlepartiesapp;
 
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.GeoPoint;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -120,4 +130,102 @@ public class PlayerController {
                 "qrSum", player.getQrSum()
         );
     }
+
+    public HashMap<String,Integer> getLeaderboardStatsFromFireBase(String find){
+        HashMap<String,Integer> qrscore = new HashMap<>();
+        HashMap<String,Integer> score = new HashMap<>();
+        HashMap<String,Integer> sum = new HashMap<>();
+
+        db.collection("Users").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot doc : task.getResult()) {
+                        String name = doc.getId();
+                        db.collection("Users").document(name).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    DocumentSnapshot doc = task.getResult();
+                                    Integer highestqr = null;
+                                    Integer qrcount = null;
+                                    Integer qrsum = null;
+                                    //Highest sum
+                                    try{
+                                        qrsum = ((Number) doc.getData().get("qrSum")).intValue();
+                                        sum.put(name,qrsum);
+                                    }catch (Exception e){
+                                        sum.put(name,0);
+                                    }
+                                    //Highest score
+                                    try{
+                                        highestqr = ((Number) doc.getData().get("qrHighest")).intValue();
+                                        score.put(name,highestqr);
+                                    }catch (Exception e){
+                                        score.put(name,0);
+                                    }
+                                    //Highest scan amount
+                                    try{
+                                        qrcount = ((Number) doc.getData().get("qrCount")).intValue();
+                                        qrscore.put(name,qrcount);
+                                    }catch (Exception e){
+                                        qrscore.put(name,0);
+                                    }
+
+
+                                    Log.d("LEADERBOARD_DEBUG", highestqr + "  " + qrcount + "  " + qrsum);
+                                   // personAdapter.notifyDataSetChanged();
+
+
+                                }
+                            }
+                        });
+                    }
+
+                }
+            }
+        });
+        if(find == "score"){
+            return score;
+        }else if(find == "sum"){
+            return sum;
+        }else if(find == "qr"){
+            return qrscore;
+        }else{
+            return null;
+        }
+
+
+    }
+
+
+
+
+
+
+
+    public static HashMap<String, Integer>
+    sortByValue(HashMap<String, Integer> hm)
+    {
+        // Create a list from elements of HashMap
+        List<Map.Entry<String, Integer> > list
+                = new LinkedList<Map.Entry<String, Integer> >(
+                hm.entrySet());
+
+        // Sort the list using lambda expression
+        Collections.sort(
+                list,
+                (i2,
+                 i1) -> i1.getValue().compareTo(i2.getValue()));
+
+        // put data from sorted list to hashmap
+        HashMap<String, Integer> temp
+                = new LinkedHashMap<String, Integer>();
+        for (Map.Entry<String, Integer> aa : list) {
+            temp.put(aa.getKey(), aa.getValue());
+        }
+        return temp;
+    }
+
+
 }
