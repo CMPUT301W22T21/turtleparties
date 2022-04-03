dd1c97b1e3ea2be1ba559692935eb24f95b2093c
Seojin3 <seojin3@ualberta.ca>
2022-04-02 23:15:13 -0600

    implemented reward system

--- turtlepartiesApp/app/src/main/java/com/example/turtlepartiesapp/GoalsFragment.java
+++ turtlepartiesApp/app/src/main/java/com/example/turtlepartiesapp/GoalsFragment.java
@@ -40,15 +40,89 @@ public class GoalsFragment extends DialogFragment {
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
 
         View view = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_goals,null);
-        ImageView goalImage = view.findViewById(R.id.goalImage);
-        TextView text = view.findViewById(R.id.goaltext);
-//        Player fragPlayer = (Player)getArguments().getSerializable("player");
+        ImageView goalImage1 = view.findViewById(R.id.goalImage1);
+        ImageView goalImage2 = view.findViewById(R.id.goalImage2);
+        ImageView goalImage3 = view.findViewById(R.id.goalImage3);
+        ImageView goalImage4 = view.findViewById(R.id.goalImage4);
+        ImageView goalImage5 = view.findViewById(R.id.goalImage5);
+        ImageView goalImage6 = view.findViewById(R.id.goalImage6);
+
+
+        Player fragPlayer = (Player)getArguments().getSerializable("player");
+        Integer qrCount = Integer.valueOf(fragPlayer.getQrCount().toString());
+
+
+        if (qrCount < 1) {
+            goalImage1.setImageResource(R.drawable.turtle1g);
+            goalImage2.setImageResource(R.drawable.turtle3g);
+            goalImage3.setImageResource(R.drawable.turtle10g);
+            goalImage4.setImageResource(R.drawable.turtle15g);
+            goalImage5.setImageResource(R.drawable.turtle20g);
+            goalImage6.setImageResource(R.drawable.turtle30g);
+        }
+
+        else if (qrCount < 3) {
+            goalImage1.setImageResource(R.drawable.turtle1);
+            goalImage2.setImageResource(R.drawable.turtle3g);
+            goalImage3.setImageResource(R.drawable.turtle10g);
+            goalImage4.setImageResource(R.drawable.turtle15g);
+            goalImage5.setImageResource(R.drawable.turtle20g);
+            goalImage6.setImageResource(R.drawable.turtle30g);
+
+        }
+        else if (qrCount < 10) {
+            goalImage1.setImageResource(R.drawable.turtle1);
+            goalImage2.setImageResource(R.drawable.turtle3);
+            goalImage3.setImageResource(R.drawable.turtle10g);
+            goalImage4.setImageResource(R.drawable.turtle15g);
+            goalImage5.setImageResource(R.drawable.turtle20g);
+            goalImage6.setImageResource(R.drawable.turtle30g);
+
+        }
+        else if (qrCount < 15) {
+            goalImage1.setImageResource(R.drawable.turtle1);
+            goalImage2.setImageResource(R.drawable.turtle3);
+            goalImage3.setImageResource(R.drawable.turtle10);
+            goalImage4.setImageResource(R.drawable.turtle15g);
+            goalImage5.setImageResource(R.drawable.turtle20g);
+            goalImage6.setImageResource(R.drawable.turtle30g);
+
+        }
+        else if (qrCount < 20) {
+            goalImage1.setImageResource(R.drawable.turtle1);
+            goalImage2.setImageResource(R.drawable.turtle3);
+            goalImage3.setImageResource(R.drawable.turtle10);
+            goalImage4.setImageResource(R.drawable.turtle15);
+            goalImage5.setImageResource(R.drawable.turtle20g);
+            goalImage6.setImageResource(R.drawable.turtle30g);
+
+        }
+        else if (qrCount < 30) {
+            goalImage1.setImageResource(R.drawable.turtle1);
+            goalImage2.setImageResource(R.drawable.turtle3);
+            goalImage3.setImageResource(R.drawable.turtle10);
+            goalImage4.setImageResource(R.drawable.turtle15);
+            goalImage5.setImageResource(R.drawable.turtle20);
+            goalImage6.setImageResource(R.drawable.turtle30g);
+
+        }
+        else {
+            goalImage1.setImageResource(R.drawable.turtle1);
+            goalImage2.setImageResource(R.drawable.turtle3);
+            goalImage3.setImageResource(R.drawable.turtle10);
+            goalImage4.setImageResource(R.drawable.turtle15);
+            goalImage5.setImageResource(R.drawable.turtle20);
+            goalImage6.setImageResource(R.drawable.turtle30);
+        }
+
+
+
 
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
         return builder
                 .setView(view)
-                .setTitle("QRcodes Scanned")
+                .setTitle("The Number of QRcodes")
                 .setPositiveButton("Close", new DialogInterface.OnClickListener() {
 
                     @Override
